9582da924471d9ada36ddf05dc6c0ca05affdbf1
lordofthesun <94736527+lordofthesun@users.noreply.github.com>
2024-02-27 19:26:50 +0600

    Update 2bot.js

--- src/commands/2bot.js
+++ src/commands/2bot.js
@@ -17,7 +17,7 @@ module.exports = {
             .setTitle('Quiz!')
             .addFields(
             { name: 'Основное', value: `> **Задержка:** \`${latency}ms\`\n> ${apiLatencyMsg}\n> **Discord.js:** \`${require('discord.js').version}\`\n> **Node.js:** \`${process.version}\``, inline: true },
-            { name: 'Дополнительно', value: `> **Разработчик:** [lordofsunshine](https://discord.com/users/1124977093546082354)\n> **Серверов:** \`${interaction.client.guilds.cache.size}\`\n> **Пользователей:** \`${interaction.client.users.cache.size}\``, inline: true }
+            { name: 'Дополнительно', value: `> **Разработчик:** [lordofsunshine](https://discord.com/users/1124977093546082354)\n> **Серверов:** \`${interaction.client.guilds.cache.size}\``, inline: true }
             )
             .setColor('#f3ae6d')
             .setThumbnail(interaction.client.user.displayAvatarURL({dynamic: true}))
