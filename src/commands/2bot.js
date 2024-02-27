3df75290c34843f54c7298ba03dab18b024e1132
lordofthesun <94736527+lordofthesun@users.noreply.github.com>
2024-02-27 19:28:36 +0600

    Update 2bot.js

--- src/commands/2bot.js
+++ src/commands/2bot.js
@@ -16,7 +16,7 @@ module.exports = {
         const pingEmbed = new EmbedBuilder()
             .setTitle('Quiz!')
             .addFields(
-            { name: 'Основное', value: `> **Задержка:** \`${latency}ms\`\n> ${apiLatencyMsg}\n> **Discord.js:** \`${require('discord.js').version}\`\n> **Node.js:** \`${process.version}\``, inline: true },
+            { name: 'Основное', value: `> **Задержка:** \`${latency}ms\`\n> ${apiLatencyMsg}\n> **Discord.js:** \`v${require('discord.js').version}\`\n> **Node.js:** \`${process.version}\``, inline: true },
             { name: 'Дополнительно', value: `> **Разработчик:** [lordofsunshine](https://discord.com/users/1124977093546082354)\n> **Серверов:** \`${interaction.client.guilds.cache.size}\``, inline: true }
             )
             .setColor('#f3ae6d')
