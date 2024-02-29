dff4e68cbd3b39a91533f2c96af584342e5f887b
lordofthesun <94736527+lordofthesun@users.noreply.github.com>
2024-02-29 17:37:11 +0600

    Update 2bot.js

--- src/commands/2bot.js
+++ src/commands/2bot.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const guild.random_quiz_interval, random_quiz_channel, rolePing = require('../models/guildModel.js');
+const { SlashCommandBuilder } = require('discord.js');
+const { random_quiz_interval, random_quiz_channel, rolePing } = require('../models/guildModel.js');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('bot')
@@ -16,12 +17,12 @@ module.exports = {
         const pingEmbed = new EmbedBuilder()
             .setTitle('Quiz!')
             .addFields(
-            { name: 'Основное', value: `> **Задержка:** \`${latency}ms\`\n> ${apiLatencyMsg}\n> **Discord.js:** \`v${require('discord.js').version}\`\n> **Node.js:** \`${process.version}\``, inline: true },
-            { name: 'Дополнительно', value: `> **Разработчик:** [lordofsunshine](https://discord.com/users/1124977093546082354)\n> **Серверов:** \`${interaction.client.guilds.cache.size}\``, inline: true },
-            { name: 'О ${guild.name}', value: `> **Канал авто-викторин:** \`${random_quiz_channel}\`\n**Интервал авто-викторин:** ${guild.random_quiz_interval}\n**Упоминаемая роль при авто-викторинах:** \`${rolePing}\` `, inline: false }
+                { name: 'Основное', value: `> **Задержка:** \`${latency}ms\`\n> ${apiLatencyMsg}\n> **Discord.js:** \`v${require('discord.js').version}\`\n> **Node.js:** \`${process.version}\``, inline: true },
+                { name: 'Дополнительно', value: `> **Разработчик:** [lordofsunshine](https://discord.com/users/1124977093546082354)\n> **Серверов:** \`${interaction.client.guilds.cache.size}\``, inline: true },
+                { name: 'О Guild', value: `> **Канал авто-викторин:** \`${random_quiz_channel}\`\n**Интервал авто-викторин:** ${random_quiz_interval}\n**Упоминаемая роль при авто-викторинах:** \`${rolePing}\` `, inline: false }
             )
             .setColor('#f3ae6d')
-            .setThumbnail(interaction.client.user.displayAvatarURL({dynamic: true}))
+            .setThumbnail(interaction.client.user.displayAvatarURL({ dynamic: true }));
 
         return interaction.reply({ embeds: [pingEmbed], ephemeral: true });
     }
